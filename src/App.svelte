e22f9228a7f1bc573d58afeeef7f8a5fe8e265c0
tanny <connecttanish@gmail.com>
2022-09-23 12:51:13 -0700

    create repo, home, setup modals

---
+++ src/App.svelte
@@ -0,0 +1,50 @@
+
+<script>
+    
+    import AboutCard from "./components/AboutCard.svelte";  
+
+
+    // state variables
+    let aboutActive = false;
+
+
+    const openResume = () => window.open("/static/resume.pdf","_blank");
+    
+
+
+</script>
+
+<div class="content-card">
+    <div class="basic-me">
+        <h1>Tanish Shah</h1>
+        <h2>Data Science student @ Simon Fraser University</h2>
+        <h2>Love Full Stack and Big Data Dev</h2>
+        <h2>Seeking Co-ops and Internships for Spring 2023</h2>
+        <div class="contact-icons">
+            <a target="_blank" href="https://github.com/t-shah02">
+                <i class="fa-brands fa-github" />
+            </a>
+            <a
+                target="_blank"
+                href="https://www.linkedin.com/in/tanish-shah-888121210/"
+            >
+                <i class="fa-brands fa-linkedin" />
+            </a>
+            <a target="_blank" href="mailto:tanish_shah@sfu.ca">
+                <i class="fa-solid fa-envelope" />
+            </a>
+        </div>
+    </div>
+    <hr />
+
+    <AboutCard/>
+
+    <div class="controls">
+        <button data-hystmodal="#about-modal">About</button>
+        <button>Projects</button>
+        <button on:click={openResume}>Resume</button>
+
+    </div>
+    <img alt="tanish shah" src="/static/tanish.jpg" />
+
+</div>
---
+++ src/components/AboutCard.svelte
@@ -0,0 +1,71 @@
+<script>
+
+  const myModal = new HystModal({
+    linkAttributeName: "data-hystmodal",
+    //settings (optional). see API
+  });
+
+</script>
+
+<div class="hystmodal" id="about-modal" aria-hidden="true">
+  <div class="hystmodal__wrap">
+    <div class="hystmodal__window" role="dialog" aria-modal="true">
+      <button data-hystclose class="hystmodal__close">Закрыть</button>
+        <h1>Hello World! 🚀 😎 📊 🌐</h1>
+        <h2>Thanks for stopping by!! 💖</h2>
+        <p>
+          I am passionate about everything from full stack web development to big data analytics.
+          Over the past year, I have been exploring these topics, through personal projects and hackathons.
+          In the future, I would love to able to combine these two fields of software engineering, and make practical applications out of them.
+        </p>
+        <p>
+          I am currently seeking internships and co-ops, that start from January (Spring 2023). I am also officially registered within the co-op program at SFU.
+          Feel free to reach out to me <a href="mailto:tanish_shah@sfu.ca">here</a> or any of my other socials, if you have any opportunities. 
+          I am extremely open and flexible to discuss with. 
+        </p>
+        <h1 class="sql-fun">
+          <span style="color : #ac8ac6">SELECT</span>
+          <span style="color : gold">*</span>
+          <span style="color : #ac8ac6">FROM</span>
+          <span style="color : gold">JOBS</span>
+          <span style="color : #ac8ac6">WHERE</span>
+          <span style="color : gold">JOB_TITLE</span>
+          <span style="color : #ac8ac6">LIKE</span>
+          <span style="color : #ac8ac6">IN</span>
+          (
+            <span style="color : lime ">"%Web%"</span>,
+            <span style="color : lime">"%Full Stack%"</span>,
+            <span style="color : lime">"%Data%"</span>,
+            <span style="color : lime">"%Analyst%"</span>
+          );
+        </h1>
+    </div>
+  </div>
+</div>
+
+<style>
+  h1 {
+    margin : 10px;
+  }
+
+  h2 {
+    font-size : 18px;
+    margin-left : 20px;
+  }
+
+  .sql-fun {
+    color : white;
+    background-color : #282c34;
+    padding : 7.5px;
+    font-size : 15px;
+  }
+
+  .sql-fun > span {
+    margin : 2.5px;
+  }
+
+  p {
+    padding : 20px;
+  }
+
+</style>
